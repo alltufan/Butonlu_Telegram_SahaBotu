dcc16e64148306778634e5dec1e1d2457db1533e
Ali TUFAN <64412965+alltufan@users.noreply.github.com>
2024-01-18 02:00:42 +0300

    Create btn_tbot_txt.py
    Türkçe karakter probleminden ötürü verileri .txt formatında kayıt alarak güncellenmiş telegram botu. 
    Ayrıca tarih saat formatı tekrar güncellenerek daha anlaşılır hale getirilmiştir.

---
+++ btn_tbot_txt.py
@@ -0,0 +1,75 @@
+
+import telebot
+from telebot import types
+from telegram.ext import Updater,CommandHandler,MessageHandler
+import datetime
+import json
+import openpyxl
+import pandas as pd
+
+
+TOKEN="6204846959:AAHudAIpNuEOcAhoQPBPUt998luaPkrDFGQ"
+bot=telebot.TeleBot(TOKEN)
+
+
+
+
+@bot.message_handler(commands=['start'])
+def start_command(message):
+    markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+    b1=types.KeyboardButton("Rapor")
+    b2=types.KeyboardButton("Arıza")
+    b3=types.KeyboardButton("Masraf")
+
+    markup.add(b1,b2,b3)
+    bot.send_message(message.chat.id,"Hoşgeldiniz!", reply_markup=markup)
+@bot.message_handler(content_types=['text'])
+def handle_text(message):
+        if message.chat.type == message.chat.type:
+            if message.text=="Rapor":
+                bot.send_message(message.chat.id,"Lütfen rapor metnini girin")
+                if message.text=="Arıza" or message.text=="Masraf":
+                     bot.send_message(message.chat.id,"Hatalı seçim")
+                else:
+                     bot.register_next_step_handler(message,report)
+            #report(message)
+            elif message.text == 'Arıza':
+                bot.send_message(message.chat.id,"Lütfen Arızayı yazın")
+                bot.register_next_step_handler(message,mulfunction)
+            elif message.text == 'Masraf':
+                bot.send_message(message.chat.id,"Lütfen Aralarında virgül koyarak TARİH, FİRMA BİLGİSİ, TUTAR şeklinde fiş bilgisini yazın")
+                bot.register_next_step_handler(message,expenses)
+
+def report(message):
+    data1= message.text
+    print(data1)
+    now1=datetime.datetime.now()
+    tsp1=now1.strftime("%Y-%m-%d %H:%M:%S")
+
+    with open('report.txt','+a') as file:
+         file.write("{} , {} \n".format(tsp1,data1))
+         file.close()
+    bot.send_message(message.chat.id,"Kayıt Alındı")
+
+def mulfunction(message):
+
+    data3= message.text
+    now = datetime.datetime.now()
+    tsp=now.strftime("%Y-%m-%d %H:%M:%S")
+    with open('Arıza.txt','+a') as file:
+         file.write("{} , {} \n".format(tsp,data3))
+         file.close()
+    bot.send_message(message.chat.id,"Arıza Kaydı oluşturuldu")
+
+def expenses(message):
+
+    data2= message.text.split(",")
+    now2=datetime.datetime.now()
+    tsp2=now2.strftime("%Y-%m-%d %H:%M:%S")
+    with open('Masraf.txt','+a') as file:
+         file.write("{},{},{} \n".format(data2[0],data2[1],data2[2]))
+         file.close()
+    bot.send_message(message.chat.id,"Masraf Girildi")
+
+
+bot.polling(non_stop=True)
